d2581148a573575575cd7d98e8da8d9f22e84153
Brett Weir <brett@lamestation.com>
2015-05-13 15:43:25 -0700

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z "$SPINC" ]
+then
+    SPINC="./openspin -u"
+fi
+
+if [ "$1" == "clean" ]
+then
+    find . -name \*.binary -exec rm {} \;
+    exit
+fi
+
+while read line
+do
+    ${SPINC} -L . "$line" >/dev/null
+    if [ $? != 0 ]
+    then 
+        echo
+        echo "${SPINC} -L . $line"
+        ${SPINC} -L . "$line"
+        echo
+    fi
+done < <(find . -name \*.spin)
+
